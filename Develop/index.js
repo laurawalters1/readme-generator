8b5bf2284d1b1801657e9ea3fcea367178c15a44
Laura Walters <laura.walters1098@yahoo.com>
2022-02-23 21:00:21 +0000

    installing prompt

--- Develop/index.js
+++ Develop/index.js
@@ -1,9 +1,23 @@
 // THEN a high-quality, professional README.md is generated with the title of my project and sections entitled Description, Table of Contents, Installation, Usage, License, Contributing, Tests, and Questions
 
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer')
+const prompt = require('prompt')
 
 // TODO: Create an array of questions for user input
+inquirer.prompt([{
+    name: 'title',
+    message: 'Please provide the title of your application'
+}])
+
+
 const questions = [
+"Please provide the title of your application",
+"Please provide a description of your application",
+"Please provide a table of contents",
+"Please provide instructions on how to install your application",
+"Please provide instructions on how to use your application",
+"Please choose a license for your application",
 
 
 ];
